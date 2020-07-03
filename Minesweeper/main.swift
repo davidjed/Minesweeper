ff1a312a92754e6f8b8ac10f91ffff4633ee29d5
David Jedeikin <djedei@gmail.com>
2020-07-03 12:31:31 -0700

    Initial Commit

---
+++ Minesweeper/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Minesweeper
+//
+//  Created by David Jedeikin on 7/3/20.
+//  Copyright © 2020 David Jedeikin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
